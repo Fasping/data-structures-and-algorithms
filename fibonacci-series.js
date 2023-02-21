6237326a309a8c8fc9406d21a7eb6877c14ce94a
Fernando Cases <nandocasesgarcia@gmail.com>
2023-02-21 11:19:34 +0100

    fibonacci serie

---
+++ fibonacci-series.js
@@ -0,0 +1,28 @@
+/*
+
+A fibonacci sequence is written as:
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+
+Fibonacci series is a series that generates subsequent series of numbers by the addition of the two previous numbers. The first two terms of the Fibonacci series are zero and one, respectively. And the next terms are the addition of the two previous terms.
+
+Example:
+
+First terms of the series is: 0
+Second term of the series is: 1
+Third terms of the series is: (0 + 1) = 1
+Fourth terms of the series is: (second + third) term = (1 + 1) = 2
+Fifth term of the series is: (Third + fourth) = 1 + 2 = 3
+
+*/
+
+function fibonacci(num) {
+  if (num == 1) return 0;
+  if (num == 2) return 1;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+let num = 7;
+answer = fibonacci(num);
+
+console.log("The 7th term of the Fibonacci series is: ", answer);
